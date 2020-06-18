0b27cef4211423b316d8170a64e03985f02cf400
thelonelinux <vickythethechguy@gmial.com>
2020-06-18 14:16:58 +0530

    Variables in Kotlin types

--- src/Package/VariablesInKotlin.kt
+++ src/Package/VariablesInKotlin.kt
@@ -1,5 +1,23 @@
 package Package
 
 fun main(){
-    
+    //Variable in kotlin
+    //var keyword for mutable keywords
+    //val keyword for immutable keywords
+    var temp:Int=10
+    println(temp)
+    temp=30
+    println(temp)
+
+    val name:String="Dhone"
+    println(name)
+
+    var i:Int
+    i=22
+    println(i)
+
+
+    //Kotlin can also self identify the data type and store the value
+    var num=50;
+    println(num)
 }
